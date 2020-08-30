22b681ef50184feef42600c63f47ad8505383ce3
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-08-30 17:08:10 -0300

    corrigido erro

--- Principal.java
+++ Principal.java
@@ -1,6 +1,6 @@
 public class Principal{
 
-    public static void name(String {}args) {
+    public static void name(String []args) {
         System.out.println ("Aprendendo git");
         
     }
