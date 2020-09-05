7808c96b5d7cb0b2acd4c89a8444a3b2f5fd199f
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-09-05 18:24:26 -0300

    Trabalhando com Strings

---
+++ src/br/com/fundamentos/TrabalhandoCOmStrings.java
@@ -0,0 +1,13 @@
+package src.br.com.fundamentos;
+
+public class TrabalhandoCOmStrings {
+
+	public static void main(String[] args) {
+		String nome = " Maria ";
+		int idade = 30;
+		
+		System.out.println(nome + " tem " + idade + " anos ");
+		
+	}
+	
+}
--- src/br/com/fundamentos/TrabalhandoComVariaveis.java
+++ src/br/com/fundamentos/TrabalhandoComVariaveis.java
@@ -1,5 +1,7 @@
 package src.br.com.fundamentos;
 
+import javax.swing.text.StyledEditorKit.BoldAction;
+
 public class TrabalhandoComVariaveis {
 	public static void main(String[] args) {
 		int quantidade; //declarando variavel inteiro
@@ -24,6 +26,27 @@ public class TrabalhandoComVariaveis {
 		
 		long numeroInteiroGrande = 25l;
 		System.out.println(" Numero inteiro com Long = " + numeroInteiroGrande);
+		
+		boolean alunoMatriculado = true;
+		boolean clienteBloquado = false;
+		
+		char turmaAluno1 = 'A';
+		char tipoCliente = '2';
+		char simbolo = '@';
+		
+		String nomePessoa = "Alicio";
+		
+		long populacaoUberlandia = 650000;
+		System.out.println(populacaoUberlandia);
+		
+		long populacaoMundial = 700000000;
+		System.out.println(populacaoMundial);
+		
+		Float saldoConta = 1030.43f;
+		System.out.println(saldoConta);
+		
+		
+		
 
 	}
 
