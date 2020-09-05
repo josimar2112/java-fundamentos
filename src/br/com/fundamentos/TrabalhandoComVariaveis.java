a4b18b647b55d2c7d8cc541a33707b25ca13a20c
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-09-05 17:16:12 -0300

    Trabalhando com variaveis

--- src/br/com/fundamentos/TrabalhandoComVariaveis.java
+++ src/br/com/fundamentos/TrabalhandoComVariaveis.java
@@ -10,6 +10,21 @@ public class TrabalhandoComVariaveis {
 		quantidade = 15;
 		
 		System.out.println(quantidade);
+		
+		// tipos primitivos https://medium.com/@mauriciogeneroso/java-oca-1z0-808-2-1-tipos-de-dados-vari%C3%A1veis-vari%C3%A1veis-primitivas-e-de-refer%C3%AAncia-c8f007756bc5
+		
+		
+		double numeroComVirgula;
+		numeroComVirgula = 1.3;
+		
+		System.out.println("Numero decimal = " + numeroComVirgula);
+		
+		float numeroComVirgulaPontoFlutuante = 1.5f;
+		System.out.println("Numero decimal com float = " + numeroComVirgulaPontoFlutuante);
+		
+		long numeroInteiroGrande = 25l;
+		System.out.println(" Numero inteiro com Long = " + numeroInteiroGrande);
+
 	}
 
 }
