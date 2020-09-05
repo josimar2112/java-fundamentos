8058ca8beac039225fc3768012c3fdc7c67d758f
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-09-05 18:54:41 -0300

    final da aula 3

--- src/br/com/fundamentos/TrabalhandoCOmStrings.java
+++ src/br/com/fundamentos/TrabalhandoCOmStrings.java
@@ -1,12 +1,13 @@
 package src.br.com.fundamentos;
 
 public class TrabalhandoCOmStrings {
-
+	//String n]ao é um tipo primitivo e sim uma classe (possui metodos)
+	
 	public static void main(String[] args) {
 		String nome = " Maria ";
 		int idade = 30;
 		
-		System.out.println(nome + " tem " + idade + " anos ");
+		System.out.println(nome.toUpperCase() + " tem " + idade + " anos ");
 		
 	}
 	
