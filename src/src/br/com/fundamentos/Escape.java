fb20e77fab99abfc2f93b10ebe283bbe7aa95ad2
Josimar Bruno Benvenutti <68384@proway.treina>
2020-09-05 15:38:35 -0300

    Adição de projeto

---
+++ src/src/br/com/fundamentos/Escape.java
@@ -0,0 +1,16 @@
+package src.src.br.com.fundamentos;
+
+public class Escape {
+	public static void main(String[] args) {
+		System.out.println("Usando o \b Eclipse");
+		System.out.println("Usando o \t Eclipse");
+		System.out.println("Usando o \n Eclipse");
+		System.out.println("Usando o \f Eclipse");
+		System.out.println("Usando o \r Eclipse");
+		System.out.println("Usando o \" Eclipse");
+		System.out.println("USando o \' Eclipse");
+		System.out.println("Usando o \\ Eclipse");
+
+	}
+
+}
