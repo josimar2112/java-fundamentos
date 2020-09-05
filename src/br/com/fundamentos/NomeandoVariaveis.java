1e440b8697fca96a30f6f99fb5478d6375ed202d
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-09-05 17:42:12 -0300

    Trabalhando com variaveis

---
+++ src/br/com/fundamentos/NomeandoVariaveis.java
@@ -0,0 +1,22 @@
+package src.br.com.fundamentos;
+
+public class NomeandoVariaveis {
+	public static void main(String[] args) {
+		//valido separado por _
+		String variavel_separada_com_underline;
+		
+		//padrao camelCase
+		String variavelSeparaComUnderline;
+		
+		//valido porem estranho
+		
+		int _numero = 1;
+		System.out.println(_numero);
+		
+		//valdo porem estranho parece PHP
+		int $outroNumero = 1;
+		System.out.println($outroNumero);
+		
+	}
+
+}
