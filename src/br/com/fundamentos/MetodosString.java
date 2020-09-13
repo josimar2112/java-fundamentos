161029b29ddbe8831eb8933f8687f57f491a72f9
Josimar Bruno Benvenutti <68384@proway.treina>
2020-09-13 16:33:19 -0300

    aula 4

---
+++ src/br/com/fundamentos/MetodosString.java
@@ -0,0 +1,14 @@
+package src.br.com.fundamentos;
+
+public class MetodosString {
+	
+	public static void main(String[] args) {
+		
+		String valor = "Trabalhando com strings ";
+		System.out.println(valor.charAt (0));
+		
+		System.out.println(valor.codePointAt(0));
+		
+	}
+
+}
--- src/br/com/fundamentos/TrabalhandoCOmStrings.java
+++ src/br/com/fundamentos/TrabalhandoCOmStrings.java
@@ -1,14 +1,61 @@
 package src.br.com.fundamentos;
 
 public class TrabalhandoCOmStrings {
-	//String n]ao é um tipo primitivo e sim uma classe (possui metodos)
-	
+	// String nao é um tipo primitivo e sim uma classe (possui metodos)
+
 	public static void main(String[] args) {
-		String nome = " Maria ";
-		int idade = 30;
-		
-		System.out.println(nome.toUpperCase() + " tem " + idade + " anos ");
-		
+		char data[] = { 'a', 'b', 'c' };
+		String str = new String(data);
+		System.out.println(str);
+
+		System.out.println();
+
+		String valor = "Trabalhando com strings ";
+		System.out.println(valor.charAt(0));
+
+		System.out.println();
+
+		System.out.println(valor.codePointAt(0));
+
+		System.out.println();
+
+		String treinamento = "Treinamento com String";
+		System.out.println(treinamento.compareTo("Treinamento com String") == 0 ? true : false);
+		System.out.println(treinamento.compareTo("TREINAMENTO COM STRING") == 0 ? true : false);
+		System.out.println(treinamento.compareToIgnoreCase("TREINAMENTO COM STRING") == 0 ? true : false);
+
+		System.out.println();
+
+		System.out.println(treinamento.endsWith("Treinamento"));
+		System.out.println(treinamento.startsWith("Trei"));
+		System.out.println(treinamento.startsWith("in", 3));
+
+		System.out.println();
+
+		System.out.println("tamanho da String -> " + treinamento.length());
+
+		// foreach exclusivo
+		for (char c : treinamento.toCharArray()) {
+			System.out.println("Char :" + c);
+		}
+		System.out.println();
+
+		for (byte b : treinamento.getBytes()) {
+			System.out.println("byte : " + b);
+		}
+		System.out.println();
+
+		String nome = "";
+		System.out.println(nome.isEmpty());
+
+		System.out.println();
+
+		String curso = "fundamento-em-java";
+		String[] valorComSplit = curso.split("-");
+
+		for (String s : valorComSplit) {
+			System.out.println(s);
+
+		}
 	}
-	
 }
